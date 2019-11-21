d061dae34e6b14bc5da334a12aac0b42f4401838
Duncan <duncan.s.mann@gmail.com>
2019-11-21 18:45:07 -0500

    Create helpers folder

---
+++ client/src/helpers/apihelpers.js
@@ -0,0 +1,29 @@
+
+import axios from 'axios'
+
+export const getArtists = async (token, arr) => {
+    try {
+
+      let queryStrings = arr.map( artist => {
+        let split = artist.split(' ')
+        return split.join('%20')
+      })
+
+      let artists = {}
+      for (let each of queryStrings) {
+        let res = await axios(`https://api.spotify.com/v1/search?q=${each}&type=artist`, {
+          type: 'GET',
+          headers: {'Authorization': 'Bearer ' + token}
+        })
+        artists[each] = res.data.artists.items[0]
+      }
+      return artists;
+      // setState(state => ({...state, artists}))
+
+    } catch (error) {
+    console.error(error)
+  }
+}
+
+
+
--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,40 +1,19 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
+import {getArtists} from '../helpers/apihelpers.js'
 
 
 export default function Dashboard(props) {
 
-const [state, setState] = useState({})
-
-const getArtists = async(token, arr) => {
-  try {
-
-  let queryStrings = arr.map( artist => {
-    let split = artist.split(' ')
-    return split.join('%20')
-  })
-
-  let artists = {}
-  for (let each of queryStrings) {
-    let res = await axios(`https://api.spotify.com/v1/search?q=${each}&type=artist`, {
-      type: 'GET',
-      headers: {'Authorization': 'Bearer ' + token}
-    })
-    artists[each] = res.data.artists.items[0]
-  }
-  console.log(artists)
-  setState(state => ({...state, artists}))
-
-} catch (error) {
-console.error(error)
-}
-}
+const [state, setState] = useState({user : {}, token: null, artists: {}})
 
 useEffect(() => {
   axios.get('/getUser')
     .then(async res => {
       setState(res.data)
-      getArtists(state.token, ['Sum 41', 'Metallica', 'Red Hot Chili Peppers'])
+      let artists = await getArtists(state.token, ['Sum 41', 'Metallica', 'Red Hot Chili Peppers'])
+
+      setState(prev => ({...prev, artists}))
     }).catch((e) => console.log('error:', e))
 }, [state.token])
 
@@ -44,3 +23,5 @@ useEffect(() => {
     </div>
     );
   }
+
+        // setState(state => ({...state, artists}))
