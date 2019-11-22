8cebf8ed387360a2748da86095f8747678294827
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-11-22 13:08:38 -0500

    connection logic for playback sdk

--- client/src/components/SongListItem.js
+++ client/src/components/SongListItem.js
@@ -92,9 +92,13 @@ export default function SongListItem(props) {
         deviceId: null
       }));
     });
-    
-
-  }
+    // Connect to the player!
+    player.connect().then(success => {
+      if (success) {
+        console.log('The Web Playback SDK successfully connected to Spotify!');
+      }
+    });
+  };
 
   return (
     <div className='SongListItem'>
