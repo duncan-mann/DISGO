571e5ff5149855f79cc25d159b39d39b6cd79735
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-11-22 11:48:43 -0500

    added useEffect on mount to load spotify playback sdk

--- client/src/components/SongListItem.js
+++ client/src/components/SongListItem.js
@@ -11,6 +11,14 @@ export default function SongListItem(props) {
   const [deviceId, SetDeviceId] = useState(null);
   const [currentPlayer, SetPlayer] = useState(null);
 
+  // On Mount, load Spotify Web Playback SDK script
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = "https://sdk.scdn.co/spotify-player.js";
+    document.head.appendChild(script);
+  }, [])
+
   return (
     <div className='SongListItem'>
       Hello World!
