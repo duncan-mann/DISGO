df673356d85a38b7f7ec727e2b68e094b2e265c6
Duncan <duncan.s.mann@gmail.com>
2019-11-21 20:50:33 -0500

    Seperated token logic and getPerformers logic in 2 use effects

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,39 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios'
-import {getArtists} from '../helpers/spotifyHelper'
+import { getArtists } from '../helpers/spotifyHelper'
 import { getPerformers } from '../helpers/seatGeekHelper'
 
 
 export default function Dashboard(props) {
 
-  const [state, setState] = useState({user : {}, token: null, artists: {}})
+  const [state, setState] = useState({ user: {}, token: null, artists: {}, events: [] })
 
-// useEffect(() => {
-//   axios.get('/getUser')
-//     .then(async res => {
-//       setState(res.data)
-//       let artists = await getArtists(state.token, ['Sum 41', 'Metallica', 'Red Hot Chili Peppers'])
 
-//       setState(prev => ({...prev, artists}))
-//     }).catch((e) => console.log('error:', e))
-// }, [state.token])
 
   useEffect(() => {
     axios.get('/getUser')
-      .then( async res => {
+      .then(async res => {
         setState(res.data)
-        if (state.token) {
-          await getPerformers().then( async performers => {
-            let artists = await getArtists(state.token, performers)
-            setState(prev => ({...prev, artists}))
-          })
-        }
       }).catch((e) => console.log('error:', e))
+  }, [])
+
+  useEffect(() => {
+    if (state.token) {
+      getPerformers()
+        .then((events) => {
+          console.log('test', events);
+          setState(prev => ({ ...prev, events }))
+        })
+    }
   }, [state.token])
+
+  //   useEffect(() => {
+  //   axios.get('/getUser')
+  //     .then(async res => {
+  //       setState(res.data)
+  //       let artists = await getArtists(state.token, ['Sum 41', 'Metallica', 'Red Hot Chili Peppers'])
+
+  //       setState(prev => ({...prev, artists}))
+  //     }).catch((e) => console.log('error:', e))
+  // }, [state.token])
 
   return (
     <div className="App">
       Token: {state.token}
     </div>
-    );
-  }
+  );
+}
