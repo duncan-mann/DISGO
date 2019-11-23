5e9c20e4c05ff398570a5e8f27cb2b6ec695f2ad
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-11-23 13:12:14 -0500

    Feature/jk 22 album cover prev next (#23)
    * create Spotify component to store playback sdk logic
    
    * extract album covers for previous and next songs

--- client/src/components/MusicControls.js
+++ client/src/components/MusicControls.js
@@ -1,11 +1,17 @@
 import React from 'react';
 
 export default function MusicControls (props) {
+  console.log(props.prevAlbumCover)
+  console.log(props.nextAlbumCover)
   
   return(
     <div className='MusicControls'>
       <h1>Music Player Controls</h1>
-      <img src={props.albumCover} />
+      <div className='albumCovers'>
+        <img src={props.prevAlbumCover} />
+        <img src={props.currentAlbumCover} />
+        <img src={props.nextAlbumCover} />
+      </div>
       <p>Song Title: {props.trackName}</p>
       <p>Artist Name: {props.artistName}</p>
       <p>Album Name: {props.albumName}</p>
