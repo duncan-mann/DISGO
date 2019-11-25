ded3e7d4711711817f6fe367187c9ba7a3fa6e21
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-11-25 13:15:58 -0500

    initialized Filter component

---
+++ client/src/components/ExportPlaylist.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PlaylistAddIcon from '@material-ui/icons/PlaylistAdd';
+
+export default function ExportPlaylist() {
+  return (
+    <div className='ExportPlaylist'>
+      <PlaylistAddIcon />
+    </div>
+  )
+}
