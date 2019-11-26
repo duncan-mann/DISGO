277513a252d813523aad0aa2d4c14542ee24f0e1
Duncan Mann <50970861+duncan-mann@users.noreply.github.com>
2019-11-26 14:31:46 -0500

    Feature/dm 35 style dash2 (#48)
    * updating helpers folder
    
    * Merging with remote master and installing new packages
    
    * Styling dashboard
    
    * Style dashboard
    
    * Add 2nd albums to next/prev albums

--- client/src/components/MusicControls.js
+++ client/src/components/MusicControls.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
 import SkipPreviousIcon from '@material-ui/icons/SkipPrevious';
 import PlayArrowIcon from '@material-ui/icons/PlayArrow';
 import PauseIcon from '@material-ui/icons/Pause';
@@ -10,15 +9,17 @@ import ShuffleIcon from '@material-ui/icons/Shuffle';
 import PlaylistAddIcon from '@material-ui/icons/PlaylistAdd';
 import VolumeUpIcon from '@material-ui/icons/VolumeUp';
 import VolumeOffIcon from '@material-ui/icons/VolumeOff';
+import { makeStyles } from '@material-ui/core/styles';
+
+
 
 const useStyles = makeStyles(theme => ({
   MusicControls : {
-    // 'background' : `linear-gradient(#212121 50%, #121212 90%)`,
+    'background' : `linear-gradient(#212121 50%, #121212 90%)`,
     'color': 'white',
     'text-align': 'center'
   },
   song: {
-    color: 'white',
     'font-size': 20
   },
   songInfo: {
@@ -42,29 +43,36 @@ const useStyles = makeStyles(theme => ({
 
 export default function MusicControls (props) {
   const classes = useStyles();
-  // console.log(props.artistName)
+
   return(
     <div className={classes.MusicControls}>
+
       <div >
-        <img className={classes.prevAlbum} src={props.prevAlbumCover} />
+        <img className={classes.prevAlbum} src={props.prevAlbumCover[1]} />
+        <img className={classes.prevAlbum} src={props.prevAlbumCover[0]} />
         <img src={props.currentAlbumCover} />
-        <img className={classes.nextAlbum} src={props.nextAlbumCover} />
+        <img className={classes.nextAlbum} src={props.nextAlbumCover[0]} />
+        <img className={classes.nextAlbum} src={props.nextAlbumCover[1]} />
       </div>
+
       <p className={classes.song}>{props.trackName}</p>
       <p className={classes.songInfo}>{props.artistName && props.artistName.join(", ")}</p>
+
       <div className='MusicControls'>
-        <SkipPreviousIcon onClick={props.handlePrev} fontSize='large' aria-label='previous' color="secondary" />
+        <SkipPreviousIcon onClick={props.handlePrev} fontSize='large' aria-label='previous' color='error' />
         {props.playing
-          ? <PauseIcon onClick={props.handleToggle} fontSize='large' aria-label='Playing' color="secondary" />
-          : <PlayArrowIcon onClick={props.handleToggle} fontSize='large' aria-label='Paused' color='secondary' />
+          ? <PauseIcon onClick={props.handleToggle} fontSize='large' aria-label='Playing' color='error' />
+          : <PlayArrowIcon onClick={props.handleToggle} fontSize='large' aria-label='Paused' color='error' />
         }
-        <SkipNextIcon onClick={props.handleNext} fontSize='large' aria-label='next' color='secondary' />
-        <ShuffleIcon fontSize='large' aria-label='shuffle' color='secondary'/>
-        <RepeatIcon onClick={props.handleRepeat} fontSize='large' aria-label='repeat' color='secondary' />
-        <RepeatOneIcon fontSize='large' aria-label='repeatOne' color='secondary' />
-        <PlaylistAddIcon fontSize='large' aria-label='export-playlist' color='secondary'/>
-        <VolumeUpIcon fontSize='large' aria-label='volume-on' color='secondary'/>
-        <VolumeOffIcon fontSize='large' aria-label='volume-off' color='secondary'/>
+
+        <SkipNextIcon onClick={props.handleNext} fontSize='large' aria-label='next' color='error' />
+        <ShuffleIcon fontSize='large' aria-label='shuffle' color='error'/>
+        <RepeatIcon onClick={props.handleRepeat} fontSize='large' aria-label='repeat' color='error' />
+        <RepeatOneIcon fontSize='large' aria-label='repeatOne' color='error' />
+        <PlaylistAddIcon fontSize='large' aria-label='export-playlist' color='error'/>
+        <VolumeUpIcon fontSize='large' aria-label='volume-on' color='error'/>
+        <VolumeOffIcon fontSize='large' aria-label='volume-off' color='error'/>
+
       </div>
     </div>
   );
