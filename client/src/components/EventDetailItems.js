ba4e04fcf896c47dddee6cca2f29b54e9e7388ef
Peter Hang <peterhanggg@gmail.com>
2019-11-28 19:05:25 -0500

    styled event details component

--- client/src/components/EventDetailItems.js
+++ client/src/components/EventDetailItems.js
@@ -1,22 +1,49 @@
 import React from 'react'
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
+import RoomIcon from '@material-ui/icons/Room';
 
 const useStyles = makeStyles(theme => ({
- event: {
-   textAlign: 'center',
-   color: 'white'
- }
+  details: {  
+    padding: "6px 6px 20px 6px",
+    textAlign: "center"
+  },
+  purchaseButton: {
+    color: 'white',
+    borderColor: 'white',
+    borderStyle: "solid",
+    fontSize: "14",
+    padding: "8px",
+    textAlign: "center",
+    borderRadius: "5px",
+    fontWeight: "bold",
+    display: "inline-block",
+    marginTop: "5px",
+    textDecoration: "none",
+    '&:hover': {
+      backgroundColor: 'white',
+      color: 'black'
+    },
+  },
+  eventDetails: {
+    color: "white"
+  },
+  address: {
+    color: "white",
+    textDecoration: "none",
+    display: "block"
+  }
 }))
 
 export default function EventDetailItems(props) {
   const classes = useStyles();
 
   return (
-    <div>
-      <p>Playing at {props.venue} </p>
-      <p>Address: {props.address} Date: {props.date} </p>
-      <p>{props.lowestPrice ? `Tickets Starting at: $${props.lowestPrice}`: "Sorry, tickets are currently sold out!"}</p>
-      <a href={props.url} target="_blank">Purchase Ticket</a>
+    <div className={classes.details}>
+      <p className={classes.eventDetails}>Playing at {props.venue} </p>
+      <a className={classes.address} href={`https://maps.google.com/maps/place?q=${props.address.split(" ").join("%")}`} target="_blank"><RoomIcon /> {props.address}</a>
+      <p className={classes.eventDetails}>Date: {props.date} </p>
+      <p className={classes.eventDetails}>{props.lowestPrice ? `Tickets Starting at: $${props.lowestPrice}`: "Sorry, tickets are currently sold out!"}</p>
+      <a href={props.url} target="_blank" className={classes.purchaseButton}>Purchase Ticket</a>
     </div>
   )
 }
--- client/src/components/EventDetails.js
+++ client/src/components/EventDetails.js
@@ -1,32 +1,30 @@
-import React, { useState, useEffect } from 'react'
-import EventDetailItems from './EventDetailItems'
-import { makeStyles } from '@material-ui/core/styles';
-import { borderRadius } from '@material-ui/system';
-
+import React, { useState, useEffect } from "react";
+import EventDetailItems from "./EventDetailItems";
+import { makeStyles } from "@material-ui/core/styles";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import "./eventDetails.css";
 
 const useStyles = makeStyles(theme => ({
-  event: {
-    'padding-top': '5%',
-    color: 'white',
-    // 'text-align': 'center',
-    'display': 'flex',
-    // 'flex-direction': 'column'
-    "justifyContent": 'space-around'
-  },
+  // event: {
+  //   "padding-top": "5%",
+  //   color: "white",
+  //   backgroundColor: "#393e46",
+  //   padding: "10px",
+  //   borderRadius: "5px"
+  // },
   artist: {
-    color: 'white',
-    width: '60%',
-    margin: '0 auto',
-    display: 'inline-block',
-    'border-bottom': '1px solid white'
-  },
-  details: {  
-    backgroundColor: "#393e46",
+    color: "white",
+    width: "60%",
+    margin: "auto",
+    borderBottom: "1px solid white",
     textAlign: "center",
-    padding: "10px",
-    borderRadius: "5px"
-  }
-}))
+    paddingTop: "3rem"
+  },
+  carousel: {
+    display: "flex",
+    justifyContent: "center",
+}}));
 
 export default function EventDetails(props) {
   const classes = useStyles();
@@ -42,23 +40,27 @@ export default function EventDetails(props) {
     return (
       <EventDetailItems
         key={index}
+        artist={e.performers[0].name}
         venue={e.venue.name}
         address={e.venue.address}
         date={e.datetime_local.split("T")[0]}
         lowestPrice={e.stats.lowest_price}
         url={e.url}
       />
-    )
-  }
-  )
-  
-  return (
-    <div className={classes.event}>
-      {/* <h1 className={classes.artist}>{props.artistName && props.artistName.join(", ")}</h1> */}
-      <div className={classes.details}>
-        {list}
-      </div>
-    </div>
+    );
+  });
 
-  )
+  return (
+    <div>
+    <h1 className={classes.artist}>{props.artistName && props.artistName.join(", ")}</h1>
+    <Carousel className={classes.carousel}
+      width={"25%"}
+      showThumbs={false}
+      showStatus={false}
+      showIndicators={false}
+    >
+      {list}
+    </Carousel>
+    </div>
+  );
 }
