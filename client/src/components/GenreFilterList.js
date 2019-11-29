e6dd8c241f89114e76a8c58a5f6bc71ea1f123f2
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-11-29 01:12:26 -0500

    removed all genre chip from filtering

--- client/src/components/GenreFilterList.js
+++ client/src/components/GenreFilterList.js
@@ -28,7 +28,6 @@ export default function Filter(props) {
     <div className={classes.root}>
       <ul>
         {list}
-        <GenreFilterListItem genreName='All'/>
       </ul>
     </div>
   );
