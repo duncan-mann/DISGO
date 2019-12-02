42e51a87008bbc4da3602153862f30d3efb2d6f0
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-12-01 23:31:32 -0500

    spacing between start and end date selectors

--- client/src/components/DateSetter.js
+++ client/src/components/DateSetter.js
@@ -1,21 +1,55 @@
 import React from "react";
 import { KeyboardDatePicker } from "@material-ui/pickers";
+import { makeStyles } from "@material-ui/core/styles";
+import Grid from '@material-ui/core/Grid';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+  },
+  startDate: {
+    width: 200,
+    // color: 'red',
+  },
+  endDate: {
+    width: 200,
+  },
+  input: {
+    color: 'grey',
+    "&:focus": {
+      color: theme.palette.secondary.main,
+    }
+  }
+}));
 
 export default function DateSetter(props) {
+  const classes = useStyles();
+
   return (
-    <div>
-      <KeyboardDatePicker
-        onChange={props.setStartDate}
-        value={props.startDate}
-        variant='inline'
-        disableToolbar
-      />
-      <KeyboardDatePicker
-        onChange={props.setEndDate}
-        value={props.endDate}
-        variant='inline'
-        disableToolbar
-      />
+    <div className={classes.root}>
+      <Grid container spacing={2} direction="row" alignItems="center" justify="center">
+        <Grid item>
+          <KeyboardDatePicker
+            className={classes.startDate}
+            onChange={props.setStartDate}
+            value={props.startDate}
+            variant='inline'
+            color='secondary'
+            disableToolbar
+            InputProps={{ classes: {input: classes.input}, }}
+          />
+        </Grid>
+        <Grid item>
+          <KeyboardDatePicker
+            className={classes.endDate}
+            onChange={props.setEndDate}
+            value={props.endDate}
+            variant='inline'
+            color='secondary'
+            disableToolbar
+            InputProps={{ classes: {input: classes.input}, }}
+          />
+        </Grid>
+      </Grid>
     </div>
   );
 }
