712c686c05f4b9c1830fa952b8445204c4a2b5b1
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-12-03 16:13:11 -0500

    netlify.toml file created

--- client/src/index.js
+++ client/src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import axios from 'axios';
 
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+
+}
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
