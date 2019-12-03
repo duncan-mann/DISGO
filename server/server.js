3615e5d8c4cebe1fce1befab9739ca32eccf67cc
Jim Kim <jim.hyunjin.kim@gmail.com>
2019-12-02 22:50:59 -0500

    Fix/jk 148 delete after filter (#149)
    * fixed navbar and fixed margins
    
    * no longer using vh to set height of dashboard components
    
    * genres with no songs left after removal are no longer part of the songsByGenre state
    
    * added comments to remove songs function

--- server/server.js
+++ server/server.js
@@ -3,8 +3,8 @@ const session = require('express-session')
 const passport = require('passport');
 const SpotifyStrategy = require('passport-spotify').Strategy;
 
-var appKey = '7544e8ff2a8646b5a82425034662a889';
-var appSecret = 'a2b8ec44058346b38b3b9ef2f8afe23c';
+var appKey = '648c7f7f959e46f8bdb43d2d0e8d3c18';
+var appSecret = '48b6945ffc9c43c79efed3b45abd8c43';
 
 let access_token = undefined;
 let current_user = undefined;
